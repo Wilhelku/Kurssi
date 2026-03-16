c64839a18a48b5f2039cf3276aee5a44782290cd
wilhelmkulinock <wilhelku@metropolia.fi>
2026-03-16 20:33:46 +0200

    .

---
+++ T5.1.py
@@ -0,0 +1,10 @@
+import random
+
+maara = int(input("Kuinka monta arpakuutiota heitetään: "))
+summa = 0
+
+for i in range(maara):
+    heitto = random.randint(1, 6)
+    summa += heitto
+
+print("Silmälukujen summa on:", summa)
---
+++ T5.2.py
@@ -0,0 +1,14 @@
+luvut = []
+
+while True:
+    syote = input("Anna luku (tyhjä lopettaa): ")
+    if syote == "":
+        break
+
+    luvut.append(float(syote))
+
+luvut.sort(reverse=True)
+
+print("Viisi suurinta lukua:")
+for luku in luvut[:5]:
+    print(luku)
