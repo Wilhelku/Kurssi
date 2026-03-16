c754061ed15e885741f6106e4d190ac4f7bbe9a8
wilhelmkulinock <wilhelku@metropolia.fi>
2026-03-16 20:25:44 +0200

    .

--- t4.4.py
+++ t4.4.py
@@ -1,5 +1,5 @@
 luku=1
-while luku<=1000:
+while luku<=1000 :
     if luku % 3 ==0:
         print(luku)
     luku+=1
--- tehtava2.py
+++ tehtava2.py
@@ -1,4 +1,4 @@
 import math
-sade = float(input("sade: "))
+ sade = float(input("sade: "))
 pinta_ala=math.pi*sade**2
 print(pinta_ala)
